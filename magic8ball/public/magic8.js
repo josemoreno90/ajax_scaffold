2cb810d69347ccc0ced0cf428d94bf5ba2d9fb03
Jose Moreno <morenojr90@yahoo.com>
2018-01-09 11:14:43 -0800

    predictions work

--- magic8ball/public/magic8.js
+++ magic8ball/public/magic8.js
@@ -1,1 +1,14 @@
 // write ajax code here
+
+const answer = document.getElementById('answer');
+
+document.getElementById('question-form')
+  .addEventListener('submit', (event) => {
+    event.preventDefault()
+    console.log("Submitted")
+    fetch('/magic8response')
+      .then(response => response.text())
+      .then(prediction => {
+        answer.innerHTML = prediction;
+  })
+})
